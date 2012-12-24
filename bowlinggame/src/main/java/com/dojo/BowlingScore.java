5e4d2e44977bdf97c7cb0a576bb5e63312c22467
hirudai <hirudai@gmail.com>
2012-12-24 12:08:34 +0900

    use Game Object version

--- bowlinggame/src/main/java/com/dojo/BowlingScore.java
+++ bowlinggame/src/main/java/com/dojo/BowlingScore.java
@@ -5,99 +5,38 @@ package com.dojo;
  * 
  */
 public class BowlingScore {
+	private Game g ;
 	public BowlingScore() {
+		g = new Game();
 	}
 
 	public int getTotalScore(String score) {
-		int totalScore = 0;
-		String sc = "";
-		String parsedScore = parseScore(score);
-		System.out.println(score);
-		System.out.println(parsedScore);
-		for (int i = 0; i < parsedScore.length(); i++) {
-			sc = parsedScore.substring(i, i + 1);
-			if (sc.matches("\\d")) {
-				totalScore += Integer.parseInt(sc);
-			} else if (sc.matches("A")) {
-				totalScore += 10;
-			}
-		}
-		return totalScore;
+		parseIntScore(score);
+		return g.score();
 	}
 
-	public String parseScore(String score) {
-		return parseStrike(parseSpare(parseMiss(score)));
-	}
-
-	public String parseMiss(String score) {
-		StringBuffer sb = new StringBuffer();
+	public void parseIntScore(String score) {
 		String sc = "";
-		for (int i = 0; i < score.length(); i++) {
-			sc = score.substring(i, i + 1);
-			if (sc.matches("-")) {
-				sb.append("0");
-			} else {
-				sb.append(sc);
-			}
-		}
-		return sb.toString();
-	}
-
-	public String parseSpare(String score) {
-		StringBuffer sb = new StringBuffer();
-		String sc = "";
-		for (int i = 0; i < score.length(); i++) {
-			sc = score.substring(i, i + 1);
-			if (sc.matches("/")) {
-				if (i - 1 >= 0) {
-					// 前の値との差分でappend
-					sb.append(10 - Integer.parseInt(score.substring(i - 1, i)));
-					if (i < score.length() - 2) {
-						// 次の文字を余分にappend
-						sb.append(score.substring(i + 1, i + 2));
-					}
-				} else {
-					// 初回が"/"だった場合の対策
-					sb.append("A");
-					if (i < score.length() - 2) {
-						// 次の文字を余分にappend
-						sb.append(score.substring(i + 1, i + 2));
-					}
-				}
-			} else {
-				sb.append(sc);
-			}
-		}
-		return sb.toString();
-	}
-
-	public String parseStrike(String score) {
-		StringBuffer sb = new StringBuffer();
-		String sc = "";
+		String preSc = "";
 		for (int i = 0; i < score.length(); i++) {
 			sc = score.substring(i, i + 1);
 			if (sc.matches("X")) {
-				if (i < score.length() - 2) {
-					sb.append("A");
-
-					if (score.substring(i + 1, i + 2).matches("X")) {
-						sb.append("A");
-					} else {
-						sb.append(score.substring(i + 1, i + 2));
-					}
-					if (score.substring(i + 2, i + 3).matches("X")) {
-						sb.append("A");
-					} else {
-						sb.append(score.substring(i + 2, i + 3));
-					}
-					if (i == score.length() - 3) {
-						break;
-					}
+				g.roll(10);
+			} else if (sc.matches("-")) {
+				g.roll(0);
+			} else if (sc.matches("/")) {
+				if (preSc.matches("\\d")) {
+					//first roll is Spare
+					g.roll(10 - Integer.parseInt(preSc));
+				}else{
+					//first roll is Spare (irregular case)
+					g.roll(5);
+					g.roll(5);
 				}
 			} else {
-				sb.append(sc);
+				g.roll(Integer.parseInt(sc));
 			}
+			preSc = sc;
 		}
-		return sb.toString();
 	}
 }
--- bowlinggame/src/test/java/com/dojo/BowlingScoreTest.java
+++ bowlinggame/src/test/java/com/dojo/BowlingScoreTest.java
@@ -10,6 +10,9 @@ import junit.framework.TestSuite;
 public class BowlingScoreTest 
     extends TestCase
 {
+	
+	private BowlingScore bs;
+
     /**
      * Create the test case
      *
@@ -20,6 +23,10 @@ public class BowlingScoreTest
         super( testName );
     }
 
+	protected void setUp() throws Exception{
+		bs = new BowlingScore();
+	}
+    
     /**
      * @return the suite of tests being tested
      */
@@ -32,17 +39,44 @@ public class BowlingScoreTest
      * Rigourous Test :-)
      */
     public void testApp()
-    {
-    	BowlingScore bs = new BowlingScore();
-
-    	assertEquals("9-9-9-9-9-9-9-9-9-9-", 90, bs.getTotalScore("9-9-9-9-9-9-9-9-9-9-") );
-        assertEquals("1/2/3/4/5/6/7/8/9/1/2", 147, bs.getTotalScore("1/2/3/4/5/6/7/8/9/1/2") );
-          	
-        assertEquals("0000000000", 0, bs.getTotalScore("0000000000") );
-        assertEquals("/111111111111111111", 29, bs.getTotalScore("/111111111111111111") );
-        assertEquals("1111111111111111111/1", 29, bs.getTotalScore("1111111111111111111/1") );
-        assertEquals("X111111111111111111", 30, bs.getTotalScore("X111111111111111111") );
-        assertEquals("111111111111111111X11", 30, bs.getTotalScore("111111111111111111X11") );       
-        assertEquals("XXXXXXXXXXXX", 300, bs.getTotalScore("XXXXXXXXXXXX") );
+    {          	
     }
+    
+    public void testGutterGame() throws Exception {
+        assertEquals("0000000000", 0, bs.getTotalScore("0000000000") );    	
+    }
+    
+	public void testAllOnes() throws Exception {
+		assertEquals("11111111111111111111",20, bs.getTotalScore("11111111111111111111"));
+	}
+    
+	public void testAllSpare()throws Exception {
+        assertEquals("1/2/3/4/5/6/7/8/9/1/2", 147, bs.getTotalScore("1/2/3/4/5/6/7/8/9/1/2") );		
+	}
+	
+	public void testAllNineGutter() throws Exception{
+    	assertEquals("9-9-9-9-9-9-9-9-9-9-", 90, bs.getTotalScore("9-9-9-9-9-9-9-9-9-9-") );	
+	}
+	
+	public void testFirstOneSpare() throws Exception{
+        assertEquals("/111111111111111111", 29, bs.getTotalScore("/111111111111111111") );
+	}
+
+	public void testLastOneSpare() throws Exception{
+        assertEquals("1111111111111111111/1", 29, bs.getTotalScore("1111111111111111111/1") );
+	}
+
+	public void testFirstOneStrike() throws Exception{
+        assertEquals("X111111111111111111", 30, bs.getTotalScore("X111111111111111111") );
+	}
+
+	public void testLastOneStrike() throws Exception{
+        assertEquals("111111111111111111X11", 30, bs.getTotalScore("111111111111111111X11") );       
+	}
+
+	
+	public void testPerfectGame() throws Exception{
+        assertEquals("XXXXXXXXXXXX", 300, bs.getTotalScore("XXXXXXXXXXXX") );
+	}
+
 }
